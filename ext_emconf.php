df7900ef88594ab39fa109d6289f23dbd4610291
eibiflo <eibiflo4@gmail.com>
2023-06-16 19:22:48 +0200

    [TASK] Updated EM conf

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF["cf_phpinclude"] = [
     'version' => '1.0.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.1.99',
+            'typo3' => '11.5.0-12.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
