b3dec4cbc327efc8f6816c51964cbc30ac62318e
eibiflo <eibiflo4@gmail.com>
2025-10-28 14:17:48 +0100

    [TASK] Fixed Issue with backward compatibility <= Typo3 12 #1

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,5 +1,11 @@
 <?php
+use TYPO3\CMS\Core\Information\Typo3Version;
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
 defined('TYPO3') || die();
+
+
+$versionInformation = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+
 
 $pluginNames = [
     'Phpfrontend',
@@ -13,11 +19,24 @@ foreach ($pluginNames as $pluginName) {
         $_LLL_db . 'tx_cfphpinclude_domain_model_phpfrontend.plugin.' . strtolower(preg_replace('/[A-Z]/', '_$0', lcfirst($pluginName)))
     );
 
-    $flexFormPath = 'EXT:cf_phpinclude/Configuration/FlexForms/' . $pluginName . 'Plugin.xml';
-    if (file_exists(\TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName($flexFormPath))) {
-        // Eigenen Content-Typ registrieren statt subtypes zu verwenden
-        $GLOBALS['TCA']['tt_content']['types'][$pluginSignature] = [
-            'showitem' => '
+    //Old Registration for TYPO3 versions <= 12
+    if($versionInformation->getMajorVersion() <= 12){
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key';
+        $flexFormPath = 'EXT:cf_phpinclude/Configuration/FlexForms/' . $pluginName . 'Plugin.xml';
+        if (file_exists(GeneralUtility::getFileAbsFileName($flexFormPath))) {
+            $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+                $pluginSignature,
+                'FILE:' . $flexFormPath
+            );
+        }
+    }else{
+        //New Registration for TYPO3 versions >= 13
+        $flexFormPath = 'EXT:cf_phpinclude/Configuration/FlexForms/' . $pluginName . 'Plugin.xml';
+        if (file_exists(GeneralUtility::getFileAbsFileName($flexFormPath))) {
+            // Eigenen Content-Typ registrieren statt subtypes zu verwenden
+            $GLOBALS['TCA']['tt_content']['types'][$pluginSignature] = [
+                'showitem' => '
                 --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
                 --palette--;;general,
                 pi_flexform,
@@ -25,13 +44,14 @@ foreach ($pluginNames as $pluginName) {
                 --palette--;;hidden,
                 --palette--;;access,
             ',
-        ];
-
-        // FlexForm dem eigenen Content-Typ zuordnen
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-            "*",
-            'FILE:' . $flexFormPath,
-            $pluginSignature
-        );
+            ];
+            // FlexForm dem eigenen Content-Typ zuordnen
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+                "*",
+                'FILE:' . $flexFormPath,
+                $pluginSignature
+            );
+        }
     }
+
 }
